a24c5b64853eff7bfea01ac6353e54701350e9db
Daphne Cuadra <dap.cuadra@duocuc.cl>
2024-06-21 10:05:28 -0400

    2

--- Noticia/settings.py
+++ Noticia/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^pf8eweucpd!a8n-icv4xk+mv5+3zkb*ar)&8y-_k7i(5xtjf7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
